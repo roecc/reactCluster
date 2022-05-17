6a7704c9c7f4fd87df1c5415932994e1bebc3060
roecc <76390093+roecc@users.noreply.github.com>
2022-05-18 01:26:29 +0200

    added iFrame, loads right model! saturation is off. :/

--- rcluster/src/App.js
+++ rcluster/src/App.js
@@ -7,6 +7,7 @@ import MarkerClusterGroup from "react-leaflet-markercluster";
 import { Icon, L } from "leaflet";
 
 import Sculptures from "./sculptures.json";
+import Sketchfab from "https://static.sketchfab.com/api/sketchfab-viewer-1.12.0.js"
 
 const mIcon = new Icon ({
   iconUrl: "https://vimune.com/assets/marker.svg",
@@ -14,51 +15,40 @@ const mIcon = new Icon ({
   className: "map-icon"
 }); 
 
-const statues = [
-  [52.517537489850895, 13.398254730072333],
-  [52.51764322256848, 13.398245864330173],
-  [52.51754843582203, 13.398469319033431],
-  [52.517652707394966, 13.398459292964683],
-  [52.51755907328436, 13.398651251223031],
-  [52.51766516401343, 13.39866734471485],
-  [52.517573762376294, 13.39885778043727],
-  [52.51767658884923, 13.398839005756894],
-  [52.48597271328849, 13.419696539640428],
-  [52.48735368591596, 13.413889557123186],
-  [52.516504869796044, 13.395531177520754],
-  [52.516504869796044, 13.395531177520754],
-  [52.49750344324148, 13.392253518104555],
-  [52.49747568362482, 13.39185118675232],
-  [52.49858442174345, 13.39211940765381],
-  [52.49861054778313, 13.391690254211428],
-  [52.51880133254509, 13.400733321905138],
-  [52.51900045159175, 13.400513380765917],
-  [52.51710225545201, 13.394541442394258],
-  [52.51711368078157, 13.39462459087372],
-  [52.51712020954001, 13.394721150398256],
-  [52.51712510610819, 13.394785523414614],
-  [52.51710225545201, 13.394541442394258],
-  [52.51711368078157, 13.39462459087372],
-  [52.51712020954001, 13.394721150398256],
-  [52.51712510610819, 13.394785523414614],
-  [52.50804593132709, 13.398672044277193],
-  [52.50550238135967, 13.402848243713379],
-  [52.51760415105653, 13.393562436103823],
-  [52.51763761055967, 13.393986225128176],
-  [52.52006866090639, 13.401578217744827],
-  [52.51934931577861, 13.393479287624361],
-  [52.51486283349881, 13.375507146120073],
-  [52.48612543090347, 13.471932560205461],
-  [52.518706669075264, 13.395158350467684],
-  [52.520643965322456, 13.397752046585085],
-  [52.51562346629126, 13.398144990205767],
-  [52.514435175802944, 13.36876541376114],
-  [52.51435682585027, 13.36904972791672],
-  [52.51434866438885, 13.369312584400179],
-  [52.51932524219855, 13.399330526590349],
-  [52.519075529030815, 13.398714959621431]
-]
+function setSketchfab () {
+  var iframe = document.getElementById( 'api-frame' );
+  var uid = 'b2f022d116834652b5a0b14f58bc8e23';
 
+  // By default, the latest version of the viewer API will be used.
+  var client = new Sketchfab( iframe );
+
+  // Alternatively, you can request a specific version.
+  // var client = new Sketchfab( '1.12.0', iframe );
+
+  client.init( uid, {
+    autostart: 1,
+
+    success: function onSuccess( api ){
+      api.start();
+      api.addEventListener( 'viewerready', function() {
+
+        // API is ready to use
+        // Insert your code here
+        console.log( 'Viewer is ready' );
+
+        api.setPostProcessing({
+            enable: false,
+            sharpenEnable: true
+        }, function() {
+            window.console.log('Post-processing filters set');
+        });
+      });
+    },
+    error: function onError() {
+        console.log( 'Viewer error' );
+    }
+  }); 
+}
 
 
 function App() {
@@ -96,7 +86,10 @@ function App() {
               icon={mIcon}
               position={sculpture.location}
             >
-              <Popup>
+              <Popup
+                onOpen={()=>window['load3D'](sculpture.sketchfabID)}
+              >
+                <iframe src="" id="api-frame" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                 <h2>
                   {sculpture.name}
                 </h2>
