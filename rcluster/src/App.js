51e8ae6e393bbdc7b244f565ec2ccf7a06bd954b
roecc <76390093+roecc@users.noreply.github.com>
2022-05-17 21:52:53 +0200

    working cluster, proper style

--- rcluster/src/App.js
+++ rcluster/src/App.js
@@ -2,28 +2,59 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
+import { Icon, L } from "leaflet";
+
+const mIcon = new Icon ({
+  iconUrl: "https://vimune.com/assets/marker.svg",
+  iconSize: [32, 28],
+  className: "map-icon"
+}); 
 
 function App() {
   return (
-    <MapContainer
-      className="markercluster-map"
-      center={[51.0, 19.0]}
-      zoom={4}
-      maxZoom={18}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
+    <div id="site-container">
+      <div className="logo-container header-blur">
+        <img class="logo" src="https://vimune.com/assets/name-white.svg" alt="">
+        </img>
+        <div class="sub-t arial">connecting the dots...</div>
+      </div>
+      <div className="box-shadow">
 
-      <MarkerClusterGroup>
-        <Marker position={[49.8397, 24.0297]} />
-        <Marker position={[52.2297, 21.0122]} />
-        <Marker position={[51.5074, -0.0901]} />
-      </MarkerClusterGroup>
-    </MapContainer>
+      </div>
+
+
+      <MapContainer 
+        className="markercluster-map"
+        center={[52.52, 13.411]} 
+        zoom={12} 
+        scrollWheelZoom={true} 
+        zoomControl={false}
+      >
+        <TileLayer
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+          url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
+        />
+
+        <MarkerClusterGroup>
+          <Marker position={[49.8397, 24.0297]} />
+          <Marker position={[52.2297, 21.0122]} />
+          <Marker position={[51.5074, -0.0901]} />
+          <Marker icon={mIcon} position={[52.51, 13.411]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+            <Marker position={[52.52, 13.421]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+        </MarkerClusterGroup>
+      </MapContainer>
+    </div>
+
   );
 }
 
